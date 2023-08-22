b94c25a49da23445b9c15b45f5afad61b2692356
Sandra Parsick <mail@sandra-parsick.de>
2023-08-22 08:58:28 +0200

    sample problem

--- 02-problem/src/test/java/com/github/sparsick/heise/awaitility/ConcurrencyDemoTest.java
+++ 02-problem/src/test/java/com/github/sparsick/heise/awaitility/ConcurrencyDemoTest.java
@@ -1,7 +1,31 @@
 package com.github.sparsick.heise.awaitility;
 
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 class ConcurrencyDemoTest {
+
+    @Test
+    void naivConcurrencyTest() {
+        var demo = new ConcurrencyDemo();
+        demo.addItem("Hello World");
+
+        while(!demo.hasNewItem()) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        assertThat(demo.allItems()).contains("Hello World");
+    }
+
+
+
 
 
 
