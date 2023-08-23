1dfe01df1566a566515837d6e88e11bfd19cbe8c
Sandra Parsick <mail@sandra-parsick.de>
2023-08-23 08:18:16 +0200

    reuse sample

--- 04-reuse-conditions/src/test/java/com/github/sparsick/heise/awaitility/ConcurrencyDemoTest.java
+++ 04-reuse-conditions/src/test/java/com/github/sparsick/heise/awaitility/ConcurrencyDemoTest.java
@@ -3,10 +3,12 @@ package com.github.sparsick.heise.awaitility;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.equalTo;
 
 
 class ConcurrencyDemoTest {
@@ -15,16 +17,17 @@ class ConcurrencyDemoTest {
     void concurrencyTest() {
         var demo = new ConcurrencyDemo();
         demo.addItem("Hello World");
+        demo.addItem("Hello Earth");
 
-        await().atMost(20, TimeUnit.SECONDS).until(() -> demo.hasNewItem());
+        await().atMost(25, TimeUnit.SECONDS).until(sizeOfItems(demo), equalTo(2));
 
 
         assertThat(demo.allItems()).contains("Hello World");
     }
 
-
-
-
+    private Callable<Integer> sizeOfItems(ConcurrencyDemo demo) {
+        return () -> demo.allItems().size();
+    }
 
 
 }
