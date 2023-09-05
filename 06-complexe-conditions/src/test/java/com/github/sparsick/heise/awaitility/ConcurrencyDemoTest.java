07562ca2f24ed56357d11c6c3051e42c5030d436
Sandra Parsick <mail@sandra-parsick.de>
2023-09-05 10:56:26 +0200

    sample complexe conditions

--- 06-complexe-conditions/src/test/java/com/github/sparsick/heise/awaitility/ConcurrencyDemoTest.java
+++ 06-complexe-conditions/src/test/java/com/github/sparsick/heise/awaitility/ConcurrencyDemoTest.java
@@ -1,22 +1,40 @@
 package com.github.sparsick.heise.awaitility;
 
 
+import org.awaitility.Durations;
+import org.awaitility.pollinterval.FibonacciPollInterval;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
+import static org.awaitility.Awaitility.given;
+import static org.awaitility.pollinterval.IterativePollInterval.iterative;
+import static org.hamcrest.Matchers.instanceOf;
 
 
 class ConcurrencyDemoTest {
+
+    @Test
+    void exceptionHandlingDemo() {
+
+//        given().ignoreExceptions().await().until(()-> new Socket("localhost", 4000) != null);
+
+    }
+
+
+
 
     @Test
     void concurrencyTest() {
         var demo = new ConcurrencyDemo();
         demo.addItem("Hello World");
 
-        await().atMost(20, TimeUnit.SECONDS).until(() -> demo.hasNewItem());
+        await().pollInterval(iterative(duration -> duration.multipliedBy(4), Duration.ofSeconds(1))).atMost(20, TimeUnit.SECONDS).until(() -> demo.hasNewItem());
 
 
         assertThat(demo.allItems()).contains("Hello World");
