1cc1a9358ee40c966a21388e18861273abb057bc
Sandra Parsick <mail@sandra-parsick.de>
2023-09-05 08:25:05 +0200

    init complexe condition

---
+++ 06-complexe-conditions/src/test/java/com/github/sparsick/heise/awaitility/ConcurrencyDemoTest.java
@@ -0,0 +1,26 @@
+package com.github.sparsick.heise.awaitility;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+
+class ConcurrencyDemoTest {
+
+    @Test
+    void concurrencyTest() {
+        var demo = new ConcurrencyDemo();
+        demo.addItem("Hello World");
+
+        await().atMost(20, TimeUnit.SECONDS).until(() -> demo.hasNewItem());
+
+
+        assertThat(demo.allItems()).contains("Hello World");
+    }
+
+
+}
